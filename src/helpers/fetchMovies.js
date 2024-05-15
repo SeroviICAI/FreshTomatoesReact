c743be017088c8db5aee5e44d95934adc7101eaa
Sergio Rodríguez Vidal <202113624@alu.comillas.edu>
2024-05-15 12:34:33 +0200

    Added Movie Details functionality

--- src/helpers/fetchMovies.js
+++ src/helpers/fetchMovies.js
@@ -1,9 +1,18 @@
 const API_URL = "https://freshtomatoesapi.onrender.com/movies";
-
+ 
 export const getMoviesList = async () => {
     const response = await fetch(`${API_URL}`);
     if (response.ok) {
-        return response.json();
+        return await response.json();
+    } else {
+        throw new Error("Error getting movies list");
+    }
+};
+ 
+export const getMovieByid = async (id) => {
+    const response = await fetch(`${API_URL}/${id}`);
+    if (response.ok) {
+        return await response.json();
     } else {
         throw new Error("Error getting movies list");
     }
--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { getMovieByid } from "../helpers/fetchMovies";
 
 const Detail = () => {
+    const { id } = useParams();
+
+    const [item, setItem] = useState(null);
+
+    useEffect(() => {
+        const getDetail = async () => {
+            const response = await getMovieByid(id);
+            setItem(response);
+            window.scrollTo(0,0);
+        }
+        getDetail();
+    }, [id]);
+
     return (
         <div>
-            Detail
+            {item ? <pre>{JSON.stringify(item, null, 2)}</pre> : 'Loading...'}
         </div>
     );
 }
